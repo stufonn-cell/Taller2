fcf19485a41185d553ae354b50e21e306c3b3c29
DexterX12 <65998342+DexterX12@users.noreply.github.com>
2025-10-27 10:44:41 -0500

    Refactor: separate API response data from logging-like data such as containerId

--- src/controllers/api/pokeneasControllerAPI.js
+++ src/controllers/api/pokeneasControllerAPI.js
@@ -8,14 +8,16 @@ function getRandomPick(arrayPokeneas) {
 function getRandomPokenea(req, res) {
   const randomPokenea = getRandomPick(pokeneas);
   const payload = {
-    id: randomPokenea.id,
-    name: randomPokenea.name,
-    height: randomPokenea.height,
-    ability: randomPokenea.ability,
+    data: {
+      id: randomPokenea.id,
+      name: randomPokenea.name,
+      height: randomPokenea.height,
+      ability: randomPokenea.ability,
+    },
     containerId: getContainerId(),
   };
 
-  res.json(randomPokenea);
+  res.json(payload);
 }
 
 module.exports = {
