30d4bc823b1eef95db98086e8ac7d34531cd0ccf
stufonn-cell <stufonn@gmail.com>
2025-11-02 09:08:19 -0500

    Fix: Port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const pokeneasApiRouter = require('./src/routes/pokeneasApi');
 const pokeneasRouter = require('./src/routes/pokeneas');
-const port = 3000;
+const port = 80;
 
 app.use(express.json());
 app.use(express.static("public"));
